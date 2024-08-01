d743e4f23545c758295bda204b91ee76f73f6fba
JSEAN6 <seannjoroge0@gmail.com>
2024-08-01 10:56:27 +0300

    add config

---
+++ config.js
@@ -0,0 +1,8 @@
+export const cloudinaryURL = "https://res.cloudinary.com/da1fegzlm/";
+export const BaseURL = "https://event-manager-backend-2xpo.onrender.com/";
+export const contactsAPI = "https://event-manager-backend-2xpo.onrender.com/api/contacts/";
+export const vendorsAPI = "https://event-manager-backend-2xpo.onrender.com/api/vendors/";
+export const csrfAPI = "https://event-manager-backend-2xpo.onrender.com/api/csrf-token/";
+export const loginAPI = "https://event-manager-backend-2xpo.onrender.com/api/login/";
+export const savetransactionAPI = "https://event-manager-backend-2xpo.onrender.com/api/save-transaction/";
+export const sendReceiptEmailAPI = "https://event-manager-backend-2xpo.onrender.com/api/send-receipt-email/";
--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Styles.css';
-import { BaseURL } from '../../config'; 
+import { BaseURL } from '../../config';
 
 
 const RegisterForm = () => {
